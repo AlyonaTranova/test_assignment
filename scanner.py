acb517e01d302e13d720081773e62c652b7b513e
AlyonaTranova <76480486+AlyonaTranova@users.noreply.github.com>
2021-04-26 13:10:59 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,44 +1,37 @@
-import socket
-import string
+import ipaddress
+import threading
+from socket import *
 
 
-class DomenSearch:
+class PortScanner:
 
-    def __init__(self, key_word):
-        self.list_of_domens = ['com', 'ru', 'net', 'org', 'info', 'cn', 'es', 'top', 'au', 'pl', 'it', 'uk', 'tk', 'ml',
-                               'ga', 'cf', 'us', 'xyz', 'top', 'site', 'win', 'bid']
-        self.all_variations = []
-        self.key_word = key_word
+    def __init__(self, addr_range):
+        self.ip_addr = addr_range
+        self.ip_addr_list = []
 
-    def create_variations(self):
-        for char in string.ascii_lowercase:
-            website = self.key_word + char
-            self.all_variations.append(website)
+    def list_of_addr(self):
+        addresses = ipaddress.ip_network(self.ip_addr)
+        for host in addresses.hosts():
+            self.ip_addr_list.append(str(host))
+        return self.ip_addr_list
 
-        _string = list(self.key_word)
-        for chars in _string:
-            new_k = self.key_word.replace(chars, "")
-            self.all_variations.append(new_k)
-
-        for index in range(len(_string) + 1):
-            new_dom = self.key_word[:index] + '.' + self.key_word[index:]
-            self.all_variations.append(new_dom)
-
-        return self.all_variations
-
-    def check_domens(self, host):
-        for dom in self.list_of_domens:
-            web = host + '.' + dom
-            try:
-                res = socket.gethostbyname(web)
-                print(web, res)
-            except:
-                pass
+    def port_scan(self, host, port):
+        s = socket(AF_INET, SOCK_STREAM)
+        s.settimeout(0.5)
+        try:
+            connection = s.connect((host, port))
+            print(f'{host} {int(port)} OPEN')
+            connection.close()
+        except:
+            pass
 
 
 if __name__ == '__main__':
-    key_word = input(str('Введите интересующее вас слово: '))
-    fishing_checking = DomenSearch(key_word=key_word)
-    list_of_hosts = fishing_checking.create_variations()
-    for host in list_of_hosts:
-        fishing_checking.check_domens(host=host)
+    ip_address = input("Введите адрес: ")
+    scanner = PortScanner(addr_range=ip_address)
+    list_of_ports = [80, 443, 22, 21, 25]
+    list_of_ip = scanner.list_of_addr()
+    for ip in list_of_ip:
+        for port in list_of_ports:
+            t = threading.Thread(target=scanner.port_scan, args=(ip, port))
+            t.start()
